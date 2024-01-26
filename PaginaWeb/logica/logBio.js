dc574fa6ff603248fc4760e8adb40e3b9dc60bfd
Informatica <informatica@alumno.local>
2024-01-26 08:08:27 -0600

    Agregando js

---
+++ PaginaWeb/logica/logBio.js
@@ -0,0 +1,10 @@
+// Contenido de script.js
+function mostrarSeccion() {
+    var seccion = document.getElementById("miSeccion");
+    seccion.style.display = "block";
+}
+
+function cerrarSeccion() {
+    var seccion = document.getElementById("miSeccion");
+    seccion.style.display = "none";
+}
